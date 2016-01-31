39e7ee752c1783d1266ca3a2d4546c8a2dfe40a7
U-ROBOT\Abhishek <abhicoders@gmail.com>
2016-01-31 18:02:15 +0530

    Add features related to high insect count and pesticide usage

--- last_man_standing/scripts/features.py
+++ last_man_standing/scripts/features.py
@@ -56,11 +56,16 @@ class FeatureTransformer(BaseEstimator):
 		Number_Doses_Week = X.Number_Doses_Week
 		Estimated_Insects_Count = X.Estimated_Insects_Count
 		Number_Weeks_Used = X.Number_Weeks_Used
-		
+		Number_Weeks_Quit = X.Number_Weeks_Quit
+
+		Currently_Using_Pesticides = (X.Pesticide_Use_Category==3) * 1.
+		Soil_Type = X.Soil_Type
+		Crop_Type = X.Crop_Type
+
 		# group_by_num_doses_week = X.groupby('Number_Doses_Week')['Estimated_Insects_Count'].mean()
 		# estimated_insects_count_per_pest = X.groupby('Pesticide_Use_Category')['Estimated_Insects_Count'].mean()
-		estimated_insect_group_per_crop_type = X.groupby('Crop_Type')['Estimated_Insects_Count'].mean()
-		estimated_insect_group_per_season = X.groupby('Season')['Estimated_Insects_Count'].mean()
+		# estimated_insect_group_per_crop_type = X.groupby('Crop_Type')['Estimated_Insects_Count'].mean()
+		# estimated_insect_group_per_season = X.groupby('Season')['Estimated_Insects_Count'].mean()
 
 		def avg_insect_count_per_dose(row):
 			num_doses = row.Number_Doses_Week
@@ -72,18 +77,22 @@ class FeatureTransformer(BaseEstimator):
 
 		def avg_insect_count_per_crop_type(row):
 			crop_type = row.Crop_Type
-			return estimated_insect_group_per_crop_type.ix[crop_type] - row.Estimated_Insects_Count
+			return int(estimated_insect_group_per_crop_type.ix[crop_type] < row.Estimated_Insects_Count)
 
 		def avg_insect_count_per_season(row):
 			season = row.Season
-			return estimated_insect_group_per_season.ix[season] - row.Estimated_Insects_Count
+			return int(estimated_insect_group_per_season.ix[season] < row.Estimated_Insects_Count)
 
 
+		insect_value_counts = X.Estimated_Insects_Count.value_counts()
+		num_doses_week_value_counts = X.Number_Doses_Week.value_counts()
 
+		Insect_Count = X.Estimated_Insects_Count.map(lambda x: insect_value_counts.ix[x])
+		Pesticide_Not_Used_Despite_High_Insect_Count = ((X.Estimated_Insects_Count > X.Estimated_Insects_Count.mean()) & (X.Pesticide_Use_Category==1)) * 1.
 		# Avg_insect_count_per_dose = X.apply(avg_insect_count_per_dose, axis=1)
 		# Avg_insect_count_per_pesticide_strategy = X.apply(avg_insect_count_per_pesticide_strategy, axis=1)
-		Avg_insect_count_per_crop_type = X.apply(avg_insect_count_per_crop_type, axis=1)
-		Avg_insect_count_per_season = X.apply(avg_insect_count_per_season, axis=1)
+		# Avg_insect_count_per_crop_type = X.apply(avg_insect_count_per_crop_type, axis=1)
+		# Avg_insect_count_per_season = X.apply(avg_insect_count_per_season, axis=1)
 
 		# Total_Dosage = Number_Doses_Week * Number_Weeks_Used
 
@@ -96,15 +105,19 @@ class FeatureTransformer(BaseEstimator):
 						 Number_Doses_Week,
 			             Estimated_Insects_Count,
 			             Number_Weeks_Used,
+			             Number_Weeks_Quit,
+			             Currently_Using_Pesticides,
+			             Soil_Type,
+			             Crop_Type,
+			             Insect_Count,
+			             Pesticide_Not_Used_Despite_High_Insect_Count,
 			             # Avg_insect_count_per_dose,
 			             # Avg_insect_count_per_pesticide_strategy,
-			             Avg_insect_count_per_crop_type,
-			             Avg_insect_count_per_season,
+			             # Avg_insect_count_per_crop_type,
+			             # Avg_insect_count_per_season,
 			             # Total_Dosage,
 			             # Zero_Weeks_Used,
 			             # Zero_Doses_Week,
-			             # Soil_Type,
-			             # Crop_Type
 			             ]).T
 	
 
